26a9a321ec77f11ca279d646cf45fc8ae90ee0cb
esjsjsj <esj.esj.sj@gmail.com>
2026-01-27 08:24:16 +0900

    Design: 구매 내역 페이지 - 마크업, css

--- app/mypage/purchases/page.tsx
+++ app/mypage/purchases/page.tsx
@@ -1,1 +1,21 @@
+'use client';
+
+import UnderBar from '@/components/common/Footer';
+import Header from '@/components/common/Header';
+import ProductList from '@/components/searchresult/ProductList';
+
 //구매내역
+export default function purchasesPage() {
+  return (
+    <>
+      <div className="font-pretendard mx-4 mb-4">
+        <Header title="판매 내역" />
+        <ProductList />
+        <ProductList />
+        <ProductList />
+        <ProductList />
+        <UnderBar />
+      </div>
+    </>
+  );
+}
