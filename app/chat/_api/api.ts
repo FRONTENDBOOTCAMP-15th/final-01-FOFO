e2d805e4131bd9ffdfc7565f481889e6d1aefe7e
kilyong jeong <uzoolove@gmail.com>
2026-02-05 18:13:54 +0900

    채팅 추가

--- app/chat/_api/api.ts
+++ app/chat/_api/api.ts
@@ -1,4 +1,5 @@
-import { ErrorRes, ServerValidationError } from '@/types/common.ts';
+import { ErrorRes, ServerValidationError } from "@/app/chat/_types/api";
+
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
